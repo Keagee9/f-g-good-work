719bdc7d85c403915986097d3b555357313bb01b
Kingsley Francis <kingsleyfrancis.kalu@gmail.com>
2025-09-17 12:54:27 +0000

    make the admin page functional

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,6 +1,7 @@
 
 import * as admin from 'firebase-admin';
 
+// This ensures we initialize the app only once, which is crucial in serverless environments.
 if (!admin.apps.length) {
   admin.initializeApp();
 }
