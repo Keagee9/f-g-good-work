56bc989b410bb3cb47f3a393db4963044e620f38
Kingsley Francis <kingsleyfrancis.kalu@gmail.com>
2025-09-17 12:09:03 +0000

    Console Error Server
    TypeError: Cannot read properties of undefined (r

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,9 +1,10 @@
 
 import * as admin from 'firebase-admin';
 
-// This prevents us from initializing the app more than once
-const app = admin.apps.length
-  ? admin.app()
-  : admin.initializeApp();
+if (!admin.apps.length) {
+  admin.initializeApp();
+}
+
+const app = admin.app();
 
 export { app };
