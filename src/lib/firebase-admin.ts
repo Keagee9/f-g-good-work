0c29976751fa541bc8ca2c8b68c9722eeee04859
Kingsley Francis <kingsleyfrancis.kalu@gmail.com>
2025-09-17 10:51:28 +0000

    Try fixing this error: `Console Error: TypeError: Cannot read properties

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -4,8 +4,6 @@ import * as admin from 'firebase-admin';
 // This prevents us from initializing the app more than once
 const app = admin.apps.length
   ? admin.app()
-  : admin.initializeApp({
-      credential: admin.credential.applicationDefault(),
-    });
+  : admin.initializeApp();
 
 export { app };
