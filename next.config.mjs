6252207b3d38188faad7b2b21ac3e91e2b56e729
Oscar Onyebuchi <onokwuruoscar101@gmail.com>
2026-02-01 19:10:49 +0100

    fix: update dev script

---
+++ next.config.mjs
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    typescript: {
+      ignoreBuildErrors: true,
+    },
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+    images: {
+      remotePatterns: [
+        { protocol: 'https', hostname: 'placehold.co', port: '', pathname: '/**' },
+        { protocol: 'https', hostname: 'picsum.photos', port: '', pathname: '/**' },
+        { protocol: 'https', hostname: 'v0-hair-salon-website-design-six.vercel.app', port: '', pathname: '/**' },
+        { protocol: 'https', hostname: 'cdn-s.acuityscheduling.com', port: '', pathname: '/**' },
+        { protocol: 'https', hostname: 'tse4.mm.bing.net', port: '', pathname: '/**' },
+        { protocol: 'https', hostname: 'tse1.mm.bing.net', port: '', pathname: '/**' },
+      ],
+    },
+  };
+  
+  export default nextConfig;
